bf0c06fc52a1d9ca008973075e9b4b2172a3e3e3
wr0ngc0degen <wr0ngc0degen@gmail.com>
2015-07-07 10:21:56 +0200

    add solution for //The Spectral Convolution Saves the Day | Step 4 Spectral Convolution Problem: Compute the convolution of a spectrum. Input: A collection of integers Spectrum. Output: The list of elements in the convolution of Spectrum. If an element has multiplicity k, it should appear exactly k times; you may return the elements in any order.

--- com/wr0ngc0degen/bioinfalgo/Antibiotics.java
+++ com/wr0ngc0degen/bioinfalgo/Antibiotics.java
@@ -103,7 +103,17 @@ public class Antibiotics
 
     public static void main(String[] args)
     {
-        from20ToMoreThan100AminoAcids("0 97 99 114 128 147 147 163 186 227 241 242 244 260 261 262 283 291 333 340 357 385 389 390 390 405 430 430 447 485 487 503 504 518 543 544 552 575 577 584 632 650 651 671 672 690 691 738 745 747 770 778 779 804 818 819 820 835 837 875 892 917 932 932 933 934 965 982 989 1030 1039 1060 1061 1062 1078 1080 1081 1095 1136 1159 1175 1175 1194 1194 1208 1209 1223 1225 1322", 1000);
+        theSpectralConvolutionSavesTheDayStep4("188 1008 950 1065 588 877 319 507 218 978 87 1081 608 202 521 0 675 764 232 289 101 689 358 246 101 925 838 1040 822 156 776 188 374 115 606 721 806 707 822 420 287 1109 533 289 432 1008 1196 907 131 590 620 402 1010 333 907 707 317 776 576 794 1065 778 420 953 489 1095 879 620 475 475 489 243 1095 964 721 271 576 418 131 101 87 691 1109 186 505 374 390 1095 663 863 909 994");
+    }
+
+    //The Spectral Convolution Saves the Day | Step 4
+    private static void theSpectralConvolutionSavesTheDayStep4(String spectrum)
+    {
+        ArrayList<Integer> convolution = spectralConvolutionProblem(spectrum);
+        for (Integer integer : convolution)
+        {
+            System.out.print(integer + " ");
+        }
     }
 
     //From 20 to More than 100 Amino Acids | Step 1
@@ -138,6 +148,26 @@ public class Antibiotics
         {
             System.out.println(peptide);
         }
+    }
+
+    public static ArrayList<Integer> spectralConvolutionProblem(String spec)
+    {
+        ArrayList<Integer> result = new ArrayList<>();
+        int[] spectrum = parseSpectrum(spec);
+        Arrays.sort(spectrum);
+        for (int i = 1; i < spectrum.length; i++)
+        {
+            int firstElement = spectrum[i];
+            for (int j = i - 1; j >= 0; j--)
+            {
+                int secondElement = spectrum[j];
+                if (firstElement - secondElement != 0)
+                {
+                    result.add(firstElement - secondElement);
+                }
+            }
+        }
+        return result;
     }
 
     public static Set<String> parsePeptides(String peptides)
